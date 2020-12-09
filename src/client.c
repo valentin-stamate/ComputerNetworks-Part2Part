8fe981d5efe9c4f76c499dae971addc3353b48d8
Valentin Stamate <stamatevalentin125@gmail.com>
2020-12-09 12:50:28 -0500

    connecting the client and the server with a working example

--- src/client.c
+++ src/client.c
@@ -1,13 +1,3 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <string.h>
-
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -15,18 +5,77 @@
 #include <string.h>
 #include <errno.h>
 #include <limits.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <dirent.h>
 #include <pwd.h>
 #include <grp.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#include <pthread.h>
+
+#define MAX_FILES 100
+
+#define GATEWAY_IP "192.168.1.9"
+#define PORT 2024
 
 extern int errno;
 
-int port;
+int main(int argc, char *argv[]) {
 
-int main (int argc, char *argv[]) {
-  
+    int sd;			
+    struct sockaddr_in server;	
+  		
+    int nr=0;
+    char buf[10];
+
+    if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror ("Eroare la socket().\n");
+        return errno;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(GATEWAY_IP);
+    server.sin_port = htons(PORT);
+    
+    
+    if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1) {
+        perror ("[client]Eroare la connect().\n");
+        return errno;
+    }
+
+    printf ("[client]Introduceti un numar: ");
+    fflush (stdout);
+    read (0, buf, sizeof(buf));
+    nr=atoi(buf);
+    
+    printf("[client] Am citit %d\n",nr);
+
+    if (write (sd,&nr,sizeof(int)) <= 0){
+        perror ("[client]Eroare la write() spre server.\n");
+        return errno;
+    }
+
+    if (read (sd, &nr,sizeof(int)) < 0) {
+        perror ("[client]Eroare la read() de la server.\n");
+        return errno;
+    }
+    
+    printf ("[client]Mesajul primit este: %d\n", nr);
+
+
+    close (sd);
+
+    return 0;
 }
+
+
+
+
+
+
--- src/server.c
+++ src/server.c
@@ -17,9 +17,25 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
+#include <pthread.h>
+
 #include "import/sql.h"
 
 #define MAX_FILES 100
+
+#define PORT 2024
+#define GATEWAY_IP "192.168.1.9"
+#define LOCAL_IP "127.0.0.1"
+
+extern int errno;
+
+typedef struct thData {
+    int idThread;
+    int cl;    
+} thData;
+
+static void *treat(void *);
+void raspunde(void *);
 
 int main(int argc, char *argv[]) {
 
@@ -28,28 +44,94 @@ int main(int argc, char *argv[]) {
     if (argc == 2) {
         initializeDatabase(db);
     }
-    
-    User u;
-    File file[MAX_FILES];
-    int nFiles = 0;
 
-    char *email = "stamatevalentin125@gmail.com";
+    struct sockaddr_in server;
+    struct sockaddr_in from;
+    int nr;
+    int sd;
+    int pid;
+    pthread_t th[100];
+    int i = 0;
 
-    u = getUserByEmail(db, email);
-    showUser(&u);
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("[server]Eroare la socket().\n");
+        return errno;
+    }
 
-    getUserFiles(db, &u, file, &nFiles);
+    int on = 1;
+    setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-    for (int i = 0; i < nFiles; i++) {
-        showFile(file + i);
+    bzero(&server, sizeof(server));
+    bzero(&from, sizeof(from));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr(GATEWAY_IP);
+
+    if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) {
+        perror("[server]Eroare la bind().\n");
+        return errno;
+    }
+
+    if (listen(sd, 2) == -1) {
+        perror("[server]Eroare la listen().\n");
+        return errno;
+    }
+
+    while (1) {
+        int client;
+        thData *td;
+        int length = sizeof(from);
+
+        printf("[server]Asteptam la portul %d...\n", PORT);
+        fflush(stdout);
+
+        if ((client = accept(sd, (struct sockaddr *)&from, &length)) < 0) {
+            perror("[server]Eroare la accept().\n");
+            continue;
+        }
+
+        td = (struct thData *)malloc(sizeof(struct thData));
+        td->idThread = i++;
+        td->cl = client;
+
+        pthread_create(&th[i], NULL, &treat, td);
     }
 
     closeDatabase(db);
     return 0;
 }
 
+static void *treat(void *arg) {
+    struct thData tdL;
+    tdL = *((struct thData *)arg);
+    printf("[thread]- %d - Asteptam mesajul...\n", tdL.idThread);
+    fflush(stdout);
+    pthread_detach(pthread_self());
+    raspunde((struct thData *)arg);
 
+    close((intptr_t)arg);
+    return (NULL);
+};
 
+void raspunde(void *arg) {
+    int nr, i = 0;
+    struct thData tdL;
+    tdL = *((struct thData *)arg);
+    if (read(tdL.cl, &nr, sizeof(int)) <= 0) {
+        printf("[Thread %d]\n", tdL.idThread);
+        perror("Eroare la read() de la client.\n");
+    }
 
+    printf("[Thread %d]Mesajul a fost receptionat...%d\n", tdL.idThread, nr);
 
+    nr++;
+    printf("[Thread %d]Trimitem mesajul inapoi...%d\n", tdL.idThread, nr);
 
+    if (write(tdL.cl, &nr, sizeof(int)) <= 0) {
+        printf("[Thread %d] ", tdL.idThread);
+        perror("[Thread]Eroare la write() catre client.\n");
+    }
+    else
+        printf("[Thread %d]Mesajul a fost trasmis cu succes.\n", tdL.idThread);
+}
